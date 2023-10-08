b1745dab431c8d29b1f577d384d0eb1765fed11a
OrlosZiming <2665923759@qq.com>
2023-10-08 22:59:36 +0800

    ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,84 @@
-import { useState } from 'react'
-import myLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import myLogo from "/vite.svg";
+import "./App.css";
+import Button from "@mui/material/Button";
+import EmailIcon from "@mui/icons-material/Email";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import { ThemeProvider } from "@mui/material/styles";
+import CSDNIcon from "/csdn.svg";
+import WXIcon from "/wx.svg";
+import theme from "./theme";
+import { Grid } from "@mui/material";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <div>
         <a href="https://mcxiaoxiao.github.io/orlosblog/" target="_blank">
           <img src={myLogo} className="logo" alt="Hole Logo" />
         </a>
       </div>
-      <h1>OrlosZiming's hole</h1>
-      <h3 className="subtitle">
-      喜欢搭积木的狗勾先生子铭的coding小窝
-      </h3>
-      <div className="card">
+
+      <h3 className="subtitle">喜欢搭积木的狗勾先生子铭的coding小窝</h3>
+      {/* <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           让我们开始吧!!!!00000000000!!11{count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      
-    </>
-  )
+      </div> */}
+      <Grid container spacing={2}>
+      <Grid item xs={12}>
+        -
+        </Grid>
+        <Grid item xs={3}>
+          <Button
+            variant="contained"
+            color="primary"
+            href="https://blog.csdn.net/qq_36001281"
+            startIcon={
+              <img src={CSDNIcon} style={{ width: "20px" }} alt="CSDNIcon" />
+            }
+          >
+            CSDN
+          </Button>
+        </Grid>
+        <Grid item xs={3}>
+          <Button
+            variant="contained"
+            color="primary"
+            href="https://github.com/mcxiaoxiao"
+            startIcon={<GitHubIcon />}
+          >
+            Github
+          </Button>
+        </Grid>
+        <Grid item xs={3}>
+          <Button
+            startIcon={<EmailIcon />}
+            variant="contained"
+            color="primary"
+            href="mailto:2665923759@qq.com"
+          >
+            Email
+          </Button>
+        </Grid>
+        <Grid item xs={3}>
+          <Button
+            variant="contained"
+            color="primary"
+            href="https://blog.csdn.net/qq_36001281"
+            startIcon={
+              <img src={WXIcon} style={{ width: "20px" }} alt="WXIcon" />
+            }
+          >
+            Wechat
+          </Button>
+        </Grid>
+        
+      </Grid>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
---
+++ src/theme.tsx
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#1a1a1a',
+    },
+    secondary: {
+      main: '#ffe4ae',
+    },
+  },
+});
+
+export default theme;
