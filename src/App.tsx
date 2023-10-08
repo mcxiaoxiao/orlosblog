61955257b8805fe0b3a21658db3fd95cfe8787be
OrlosZiming <2665923759@qq.com>
2023-10-08 19:56:18 +0800

    try

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>OrlosZiming</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           让我们开始吧{count}
