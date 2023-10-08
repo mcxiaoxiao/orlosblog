121891922b92b64ea7decfafab0125d78ea390a0
OrlosZiming <2665923759@qq.com>
2023-10-08 19:43:16 +0800

    ho

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/orlosblog/",
+  base: "./",
   build: {
     outDir: 'docs' // 打包文件的输出目录
   }
